72e5e0974491ab17cbe522ad03acb5b0a8a26381
Leejun <823974987@qq.com>
2015-11-20 19:36:28 +0800

    注册 登录 接口

--- SalesiPhone/ViewControllers/Login/WelcomeViewController.m
+++ SalesiPhone/ViewControllers/Login/WelcomeViewController.m
@@ -9,11 +9,24 @@
 #import "WelcomeViewController.h"
 #import "AppDelegate.h"
 
+#import "ZLoginService.h"
+#import "ZUtility.h"
+#import "ZArchive.h"
+#import "ZUserDTO.h"
+#import "MBProgressHUD.h"
+#import "ZItemService.h"
+#import "ZDataCache.h"
+#import "ZMacAddress.h"
+#import "ZRegisterDeviceDTO.h"
+#import "ZOthersService.h"
+
 @interface WelcomeViewController ()
 
 @property (nonatomic, strong) IBOutlet UITextField *accountTextField;
 @property (nonatomic, strong) IBOutlet UITextField *passwordTextField;
 @property (nonatomic, strong) IBOutlet UIView *contentView;
+
+@property (nonatomic, strong) MBProgressHUD* HUD;
 
 - (IBAction)skimAction:(id)sender;
 - (IBAction)loginAction:(id)sender;
@@ -39,6 +52,9 @@
                                              selector:@selector(keyboardWillHide:)
                                                  name:UIKeyboardWillHideNotification
                                                object:nil];
+    
+    [self sendLogin];
+//    [self registDevice:nil];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -113,6 +129,106 @@
     
     // commit animations
     [UIView commitAnimations];
+}
+
+-(void)sendLogin{
+    
+    ZLoginService *zs = [[ZServiceFactory sharedService]getLoginService];
+    [zs logout];
+    NSString* name = @"000";
+    NSString* pwd = @"123456";
+    if([name length]==0|| [pwd length]<6)
+    {
+        [ZUtility showAlert:@"请检查用户名和密码输入是否正确！"];
+        return;
+    }
+    [zs login:name pwd:pwd ifDemo:NO type:self];
+}
+
+-(void)registDevice:(UIButton*)btn
+{
+    NSString *appId = [ZMacAddress getDeviceId];
+    ZRegisterDeviceDTO* dto = [[ZRegisterDeviceDTO alloc]init];
+    dto.appid = appId;
+    dto.verifyCode = @"343381";
+    dto.shopType = [NSNumber numberWithInt:100];//服装批发商
+    
+    ZOthersService* service = [[ZServiceFactory sharedService]getOtherService];
+    [service registerDeviceWithCode:dto type:self];
+}
+
+- (void)handleResponse:(ZResponse*)response
+{
+    switch (response.businessType) {
+            
+        case kLogin:
+            if (response.code.code == 200 || response.code.code == 204) {
+                if ([response.respObj isKindOfClass:[ZUserDTO class]]){
+                    ZUserDTO* dto = (ZUserDTO*)response.respObj;
+                    [ZDataCache sharedInstance].userDto = dto;
+                    [ZResourceMgr shopType:[dto.shopType intValue]];
+                    [[ZArchive instance] saveShopID:dto.shopId];
+                    [[ZArchive instance] updateShopSettingString:40 key:kToken value:dto.token];
+                    
+//                    [self sendImportRequestBeforAll];
+//                    NSString* name = [((UITextField*)[self viewWithTag:kTagUsername]) text];
+//                    NSString* pwd = [((UITextField*)[self viewWithTag:kTagPassword]) text];
+//                    NSUserDefaults *accountDefaults = [NSUserDefaults standardUserDefaults];
+//                    [accountDefaults setObject:name forKey:kUserName];
+//                    [accountDefaults setObject:pwd forKey:kPassword];
+//                    [accountDefaults synchronize];
+                }
+            } else if(response.code.code) {
+                
+                [self showErrorResponse:response];
+                
+            } else {
+                
+                NSString* text = [NSString stringWithFormat:@"登录失败, 请稍后重试。"];
+                UIAlertView* alert = [[UIAlertView alloc] initWithTitle:@"提示"
+                                                                message:text
+                                                               delegate:self
+                                                      cancelButtonTitle:@"OK"
+                                                      otherButtonTitles:nil, nil];
+                
+                [alert show];
+                
+                
+            }
+            break;
+        case kRegisterDevice:
+            if (response.code.code == 200 || response.code.code == 204) {
+                [ZUtility showAlert:@"感谢您注册使用商得乐(SDL)IPad销售软件。请查看 微信 上您获得的账号！    掌淘科技"];
+                [ZUtility endEdit:self.view];
+                NSString* venderId = [ZMacAddress getDeviceId];
+                [[ZArchive instance]updateRegistedIdenifierForVendor:venderId];
+            } else if(response.code.code) {
+                [self showErrorResponse:response];
+            }
+            break;
+    }
+    
+}
+
+-(void)sendImportRequestBeforAll
+{
+    _HUD = [[MBProgressHUD alloc] initWithView:self.view];
+    [self.view addSubview:_HUD];
+    _HUD.labelText = @"Loading";
+    _HUD.removeFromSuperViewOnHide = YES;
+    [_HUD show:YES];
+    ZItemService* itemservice = [[ZServiceFactory sharedService]getItemService];
+    [itemservice queryBaseData:self showLoading:NO];
+    
+    
+}
+
+-(void)showErrorResponse:(ZResponse *)errorResp
+{
+    if (errorResp.code.code == 406) {
+        [ZUtility showAlert:errorResp.text];
+    }
+    
 }
 
 @end
